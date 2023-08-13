4034b06892425f1df8e21162ec52e85733caa587
Utkarsh Upadhyay <118432516+utk2103@users.noreply.github.com>
2023-08-14 00:14:21 +0530

    Input

---
+++ Main2.java
@@ -0,0 +1,13 @@
+package Utkarsh;
+//For output
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        //System.out.println("Hey how are you?");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextInt());
+    }
+
+}
