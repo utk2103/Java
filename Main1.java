6be89f18b00adc9b11214e79a1b096b207394bf0
Utkarsh Upadhyay <118432516+utk2103@users.noreply.github.com>
2023-08-14 00:13:56 +0530

    Output

---
+++ Main1.java
@@ -0,0 +1,9 @@
+package Utkarsh;
+
+//for input
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.print("You look fuking handsome ");//if you remove ln on same line next statement got printed
+        System.out.println("When you are smiling");
+    }
+}
