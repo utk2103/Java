25f41a808034e73a2c9e9f65496fd0f8a675a5b0
Utkarsh Upadhyay <118432516+utk2103@users.noreply.github.com>
2023-08-15 01:36:32 +0530

    typecasting of datatypes

---
+++ TypeCasting.java
@@ -0,0 +1,12 @@
+package Utkarsh;
+
+import java.util.Scanner;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float num = input.nextFloat();
+        System.out.println(num);
+
+    }
+}
