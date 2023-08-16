e16edf8d039417c29ea0248eb36c64a2e763183f
Utkarsh Upadhyay <118432516+utk2103@users.noreply.github.com>
2023-08-17 01:52:44 +0530

    Basic typecasting

--- TypeCasting.java
+++ TypeCasting.java
@@ -8,5 +8,24 @@ public class TypeCasting {
         float num = input.nextFloat();
         System.out.println(num);
 
+
+        //narrow conversion -> int into float i.e.small into big
+        int num = (int)(67.56f);
+        System.out.println(num);
+
+        // Automatic type promotion in expressions
+        int a = 234;
+        byte b = (byte)(a);
+        System.out.println(b);  // 243 % 242 = 1
+
+        byte a = 40;
+        byte b = 50;
+        byte c =100;
+        int d - =(a * b) / c;
+
+        System.out.println(d);
+        
+        
+
     }
 }
