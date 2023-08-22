d49d73d2811337061f7f7d539d724553fc1bbb91
Utkarsh Upadhyay <118432516+utk2103@users.noreply.github.com>
2023-08-22 15:59:49 +0530

    added the program

--- Conditions_loops/Main.java
+++ Conditions_loops/Main.java
@@ -1,7 +1,23 @@
 package Conditions_loops;
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) 
+       {
+        int salary = 25400;
+//        if (salary > 10000){
+//            salary = salary + 2000;
+//        }else {
+//            salary = salary +1000;
+//        }
+
+        // Multiple if-else
+        if (salary > 20000){
+            salary += 3000; //salary = salary + 2000
+        } else if (salary > 10000) {
+            salary += 2000;
+        } else {
+            salary += 1000;
+        }
+        System.out.println(salary);
     }
 
 }
