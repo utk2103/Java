7d48565250129b0ab76ae458aae3c84042136089
Utkarsh Upadhyay <118432516+utk2103@users.noreply.github.com>
2023-08-23 20:15:38 +0530

    New format switch statements

--- Switch/switchcase.java
+++ Switch/switchcase.java
@@ -5,23 +5,36 @@ import java.util.Scanner;
 public class switchcase {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String fruit = in.next();
+//        String fruit = in.next();
+//
+//        switch (fruit) {
+//            case "Mango" -> System.out.println("King of the fruit");
+//            case "Apple" -> System.out.println("A sweet red fruit");
+//            case "Orange" -> System.out.println("Round fruit");
+//            case "Grapes" -> System.out.println("Small fruit");
+//            default -> System.out.println("Enter the valid fruit");
+//
+//        }
 
-        switch (fruit){
-            case "Mango":
-                System.out.println("King of the fruit");
-                break;
-            case "Apple":
-                System.out.println("A sweet red fruit");
-                break;
-            case "Orange":
-                System.out.println("Round fruit");
-                break;
-            case "Grapes":
-                System.out.println("Small fruit");
-                break;
-            default:
-                System.out.println("Enter the valid fruit");
+//        int day = in.nextInt();
+//        switch(day){
+//            case 1 -> System.out.println("Monday");
+//            case 2 -> System.out.println("Tuesday");
+//            case 3 -> System.out.println("Wednesday");
+//            case 4 -> System.out.println("Thursday");
+//            case 5 -> System.out.println("Friday");
+//            case 6 -> System.out.println("Saturday");
+//            case 7 -> System.out.println("Sunday");
+//        }
+
+        // Case where we do not want break
+        int day = in.nextInt();
+        switch (day) {
+            case 1, 2, 3, 4, 5 -> System.out.println("Weekdays");
+            case 6, 7 -> System.out.println("Weekend");
         }
+
+        // Nested switch cases
+
     }
 }
