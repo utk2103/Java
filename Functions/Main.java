06587b82d41a77b95c5e161792cd21f02254d9f2
Utkarsh Upadhyay <118432516+utk2103@users.noreply.github.com>
2023-08-25 19:02:23 +0530

    Updated code

--- Functions/Main.java
+++ Functions/Main.java
@@ -1,4 +1,26 @@
 package Functions;
 
+import java.util.Scanner;
+
 public class Main {
-}
+
+    public static void main(String[] args) {
+        // Q: take input of 2 numbers and print the num
+
+//        Scanner in = new Scanner(System.in);
+//        int a = in.nextInt();
+//        int b = in.nextInt();
+//        System.out.println("Enter the numbers:");
+//        int sum = a + b;
+//        System.out.println(sum);
+
+        /*
+
+        access modifier (concept of oops) return_type (){
+        // body
+        return statement;
+        }
+
+         */
+    }
+} 
