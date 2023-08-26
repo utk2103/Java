7158aa8723499581b30fd08e5956d79a68dd880a
Utkarsh Upadhyay <118432516+utk2103@users.noreply.github.com>
2023-08-26 21:40:12 +0530

    Add files via upload

---
+++ Functions/Greeting.java
@@ -0,0 +1,10 @@
+package Functions;
+
+public class Greeting {
+    public static void main(String[] args) {
+
+    }
+    static void greeting(){
+        System.out.println("Hello world!");
+    }
+}
