78b5c04a3670e6fafc9269b0e3b893ebe7c5fd02
Utkarsh Upadhyay <118432516+utk2103@users.noreply.github.com>
2023-08-27 22:04:07 +0530

    Add files via upload

---
+++ Functions/Stringexample.java
@@ -0,0 +1,14 @@
+package Functions;
+
+public class Stringexample {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+
+    static String greet(){
+
+        String greeting = "how are you";
+        return greeting;
+    }
+}
