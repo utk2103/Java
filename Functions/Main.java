974a7a0003fd5f2fd9869790bce0bcb2e8ec65ae
Utkarsh <btoshine774@gmail.com>
2023-10-30 23:19:45 +0530

    Removed cmnt and organize files

--- Functions/Main.java
+++ Functions/Main.java
@@ -7,12 +7,12 @@ public class Main {
     public static void main(String[] args) {
         // Q: take input of 2 numbers and print the num
 
-//        Scanner in = new Scanner(System.in);
-//        int a = in.nextInt();
-//        int b = in.nextInt();
-//        System.out.println("Enter the numbers:");
-//        int sum = a + b;
-//        System.out.println(sum);
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println("Enter the numbers:");
+        int sum = a + b;
+        System.out.println(sum);
 
         /*
 
--- Prime_no.java
+++
@@ -1,7 +0,0 @@
-package Utkarsh;
-
-public class Prime_no{
-    public static void main(String[] args) {
-
-    }
-}
