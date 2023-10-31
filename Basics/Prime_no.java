f38c3b9b7bb43d1b53422e46078f6d9a430041a3
Utkarsh Upadhyay <118432516+utk2103@users.noreply.github.com>
2023-11-01 02:32:38 +0530

    Added Untracked files before

---
+++ Basics/Prime_no.java
@@ -0,0 +1,7 @@
+package Utkarsh;
+
+public class Prime_no{
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Basics/primitives.java
@@ -0,0 +1,17 @@
+package Utkarsh;
+
+public class primitives {
+    public static void main(String[] args) {
+        //Primitive are that datatype which you cannot break further
+        int rollno =64;
+        char letter = 'r';
+        float marks = 98.45f;
+        double largeDecimalNumbers = 234242.4434;
+        long largeInteger = 232433243232343255L;
+        boolean check = false;
+
+        // String is not primitive
+        String name = "Utkarsh";
+
+    }
+}
